bfee18f4821d97412b40bc86fef1c8f5921817bb
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2024-12-26 08:34:59 -0300

    fist commit

---
+++ src/test/java/com/PageMarkerSite/PageMarker/PageMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PageMarkerSite.PageMarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
