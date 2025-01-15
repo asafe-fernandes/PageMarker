cec590bed38eb8932b6e9482cf304b902d863f73
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-01-15 17:47:38 -0300

    added more logs and swapped post to put

--- src/main/java/com/PageMarkerSite/PageMarker/controllers/MangaController.java
+++ src/main/java/com/PageMarkerSite/PageMarker/controllers/MangaController.java
@@ -37,9 +37,10 @@ public class MangaController {
     return ResponseEntity.ok().body(mangas);
   }
 
-  @PostMapping
+  @PutMapping
   public ResponseEntity<Manga> insert(@RequestBody MangaDTO mangaDTO) {
     Manga newManga = this.mangaService.insert(mangaDTO);
+    System.out.println("mangaDTO=" + mangaDTO.toString());
     System.out.println(newManga.toString());
     return ResponseEntity.ok().body(newManga);
   }
@@ -47,13 +48,15 @@ public class MangaController {
   @DeleteMapping("/{id}")
   public ResponseEntity<Manga> delete(@PathVariable("id") String id) {
     Manga deletedManga = this.mangaService.delete(id);
+    System.out.println("mangaDTO=" + mangaDTO.toString());
     System.out.println(deletedManga.toString());
     return ResponseEntity.ok().body(deletedManga);
   }
 
-  @PutMapping("/{id}")
+  @PostMapping("/{id}")
   public ResponseEntity<Manga> update(@PathVariable("id") String id, @RequestBody MangaDTO mangaDTO) {
     Manga updatedManga = this.mangaService.update(id, mangaDTO);
+    System.out.println("mangaDTO=" + mangaDTO.toString());
     System.out.println(updatedManga.toString());
     return ResponseEntity.ok().body(updatedManga);
 
