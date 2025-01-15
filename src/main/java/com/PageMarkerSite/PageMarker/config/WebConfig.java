725cd9e525dd670332ab102cc55e0d6ee7eb7416
asafe fernandes <177295792+asafe-fernandes@users.noreply.github.com>
2025-01-15 20:57:38 -0300

    some changes

--- src/main/java/com/PageMarkerSite/PageMarker/config/WebConfig.java
+++ src/main/java/com/PageMarkerSite/PageMarker/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**") // Apply to all endpoints
-        .allowedOrigins("http://localhost:5173") // Allow your frontend's origin
+        .allowedOriginPatterns("*") // Allow your frontend's origin
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these methods
         .allowedHeaders("Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers")
         .allowCredentials(false) // Optional: if you need to allow cookies/credentials
